6234b615586ff90ceaf1c5285f47922124427737
Rollie Ma <rollie.ma@gmail.com>
2014-12-05 23:53:10 -0800

    Added bootstrap 3.3.1

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -2,7 +2,8 @@ require.config({
     paths: {
         jquery: 'libs/jquery-1.11.1.min',
         underscore: 'libs/underscore-1.7.0.min',
-        backbone: 'libs/backbone-1.1.2.min'
+        backbone: 'libs/backbone-1.1.2.min',
+        bootstrap: 'libs/bootstrap-3.3.1.min'
     }
 });
 
