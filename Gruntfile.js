50977548b559cbfbe1084d43ea52a3d9d3c322df
Rollie Ma <rollie.ma@gmail.com>
2014-12-07 00:44:22 -0800

    Added file head to Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,12 @@
+/*
+ * tetris
+ *
+ * Personal site built on top of node.js, express and backbone.js
+ * https://github.com/waltzofpearls/tetris
+ *
+ * Copyright (c) 2014 Topbass Labs (topbasslabs.com)
+ * Author Waltz.of.Pearls <rollie@topbasslabs.com, rollie.ma@gmail.com>
+ */
 
 module.exports = function(grunt) {
     'use strict';
