19dad8099574d3f88361ff2bb4da581a2e8bf322
Rollie Ma <rollie.ma@gmail.com>
2014-12-06 23:34:04 -0800

    Changed Gruntfile to Gruntfile.js and added task loading code

---
+++ Gruntfile.js
@@ -0,0 +1,10 @@
+
+module.exports = function(grunt) {
+    'use strict';
+
+    grunt.initConfig({
+    });
+
+    require('load-grunt-tasks')(grunt, { scope: 'devDependencies' });
+    require('time-grunt')(grunt);
+};
