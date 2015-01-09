17e13df6f49562dc53403a63f464157ddb046afa
Rollie Ma <rollie.ma@gmail.com>
2015-01-09 01:59:48 -0800

    Added about/projects/resume routes and views

--- app.js
+++ app.js
@@ -5,9 +5,6 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
 var app = express();
 
 // view engine setup
@@ -22,8 +19,9 @@ app.use(cookieParser());
 app.use(require('less-middleware')(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+app.use('/', require('./routes/index'));
+app.use('/api', require('./routes/api'));
+app.use('/users', require('./routes/users'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,16 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Tetris' });
 });
 
+router.get('/about', function(req, res) {
+  res.render('about', { title: 'Tetris' });
+});
+
+router.get('/projects', function(req, res) {
+  res.render('projects', { title: 'Tetris' });
+});
+
+router.get('/resume', function(req, res) {
+  res.render('resume', { title: 'Tetris' });
+});
+
 module.exports = router;
