747a45639513ebc7162bff95eeee4b8e30544fc0
Rollie Ma <rollie.ma@gmail.com>
2015-01-10 23:59:52 -0800

    Added some backbone routes

--- public/javascripts/router.js
+++ public/javascripts/router.js
@@ -6,17 +6,31 @@ define([
     var AppRouter = Backbone.Router.extend({
         routes: {
             // Define some URL routes
-            // '/projects': 'showProjects',
-            // '/users': 'showUsers',
+            '/about': 'showAbout',
+            '/projects': 'showProjects',
+            '/resume': 'showResume',
             // Default
             '*actions': 'defaultAction'
         }
     });
 
     var initialize = function() {
-        var app_router = new AppRouter();
+        var router = new AppRouter();
 
-        app_router.on('defaultAction', function(actions) {
+        // app_router.on('showProjects', function(){
+        //   // Call render on the module we loaded in via the dependency array
+        //   // 'views/projects/list'
+        //   var projectListView = new ProjectListView();
+        //   projectListView.render();
+        // });
+        //   // As above, call render on our loaded module
+        //   // 'views/users/list'
+        // app_router.on('showUsers', function(){
+        //   var userListView = new UserListView();
+        //   userListView.render();
+        // });
+
+        router.on('defaultAction', function(actions) {
             // We have no matching route, lets just log what the URL was
             console.log('No route:', actions);
         });
