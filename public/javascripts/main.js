5b7a87c1dac9abd043cd53c30b174ebe9c7f52e0
Rollie Ma <rollie.ma@gmail.com>
2015-01-11 23:13:32 -0800

    Added font-end template files

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -6,7 +6,8 @@ require.config({
         jquery: 'libs/jquery-1.11.1',
         underscore: 'libs/underscore-1.7.0',
         backbone: 'libs/backbone-1.1.2',
-        bootstrap: 'libs/bootstrap-3.3.1'
+        bootstrap: 'libs/bootstrap-3.3.1',
+        templates: '../templates'
     }
 });
 
