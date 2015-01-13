187b02657fb7e95b802f129ff39080318f8c66ba
Rollie Ma <rollie.ma@gmail.com>
2015-01-13 00:53:02 -0800

    Changed node/express routes to match all except for home page

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+// GET home page
 router.get('/', function(req, res) {
   res.render('index', { title: 'Tetris' });
 });
 
-router.get('/about', function(req, res) {
-  res.redirect('/#about');
-});
-
-router.get('/projects', function(req, res) {
-  res.redirect('/#projects');
-});
-
-router.get('/resume', function(req, res) {
-  res.redirect('/#resume');
+// GET about|projects|resume pages
+router.get('*', function(req, res) {
+    res.redirect(req.url.replace(/^\/?/, '/#'));
 });
 
 module.exports = router;
