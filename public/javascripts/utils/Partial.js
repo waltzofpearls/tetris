ad76348905f96b8ab554cbef377ca5f55cfc2f5e
Rollie Ma <rollie.ma@gmail.com>
2015-02-15 14:22:26 -0800

    Added Partial util class

---
+++ public/javascripts/utils/Partial.js
@@ -0,0 +1,28 @@
+
+define([
+    'jquery',
+    'underscore',
+    'backbone',
+], function($, _, Backbone) {
+    return {
+        bootstrap: function() {
+            var img = $('img');
+
+            _.each(this.preload, function(src) {
+                img.clone().attr('src', src);
+            });
+        },
+
+        preload: {
+            loader: '/images/spinner1.gif',
+        },
+
+        template: {
+            loader: '\
+                <div class="col-md-12 tetris-loader">\
+                    <img src="<%= loader %>"> Loading...\
+                </div>\
+            '
+        }
+    };
+});
