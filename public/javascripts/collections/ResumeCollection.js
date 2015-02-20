7a0a3f8fef40117a9374e520ca01445939d853f3
Rollie Ma <rollie.ma@gmail.com>
2015-02-20 01:25:43 -0800

    Added ResumeModel and ResumeCollection for backbone

---
+++ public/javascripts/collections/ResumeCollection.js
@@ -0,0 +1,12 @@
+define([
+    'underscore',
+    'backbone',
+    'models/ResumeModel'
+], function(_, Backbone, ResumeModel) {
+    var ResumeCollection = Backbone.Collection.extend({
+        model: ResumeModel,
+        url: '/javascripts/data/resume.json'
+    });
+
+    return ResumeCollection;
+});
---
+++ public/javascripts/models/ResumeModel.js
@@ -0,0 +1,9 @@
+define([
+    'underscore',
+    'backbone'
+], function(_, Backbone) {
+    var ResumeModel = Backbone.Model.extend({
+    });
+
+    return ResumeModel;
+});
