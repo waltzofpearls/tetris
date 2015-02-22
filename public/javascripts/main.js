c51d0c4695ef55fcf5454d7b4d0ee37c42e75dce
Rollie Ma <rollie.ma@gmail.com>
2015-02-21 23:55:32 -0800

    Imported marked lib to front-end framework

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -5,11 +5,12 @@ require.config({
         'bootstrap': {'deps': ['jquery']}
     },
     paths: {
-        jquery: 'libs/jquery-1.11.1',
-        underscore: 'libs/underscore-1.7.0',
         backbone: 'libs/backbone-1.1.2',
         bootstrap: 'libs/bootstrap-3.3.1',
-        templates: '../templates'
+        jquery: 'libs/jquery-1.11.1',
+        marked: 'libs/marked-0.3.3',
+        templates: '../templates',
+        underscore: 'libs/underscore-1.7.0'
     }
 });
 
