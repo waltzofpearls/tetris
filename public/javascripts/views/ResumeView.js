d3c6588af83e044b0832d0290f145a86bf50512a
Rollie Ma <rollie.ma@gmail.com>
2015-02-22 19:20:20 -0800

    Completed resume page

--- public/javascripts/views/ResumeView.js
+++ public/javascripts/views/ResumeView.js
@@ -10,23 +10,44 @@ define([
     'text!templates/resumeTemplate.html'
 ], function($, _, Backbone, Marked, Partial, ResumeCollection, resumeTemplate) {
     var ResumeView = Backbone.View.extend({
-        el: $('.tetris-main-container'),
+        tagName: 'div',
+        className: 'tetris-page-resume',
+
+        xhr: null,
+
+        initialize: function(options) {
+            this.app = options.app;
+            this.tube = options.tube;
+        },
 
         render: function() {
             var that = this;
 
             this.collection = new ResumeCollection();
-            this.collection.fetch({
+            this.xhr = this.collection.fetch({
                 success: function(collection, response) {
                     that.$el.html(_.template(resumeTemplate)({
                         _: _,
                         Marked: Marked,
                         resume: collection.models[0]
                     }));
+                    that.xhr = null;
                 }
             });
 
             this.$el.html(_.template(Partial.template.loader)(Partial.preload));
+
+            return this;
+        },
+
+        close: function() {
+            if (this.xhr !== null) {
+                this.xhr.abort();
+            }
+            _.each(this.subViews, function(view) {
+                view.remove();
+            });
+            this.remove();
         }
     });
 
