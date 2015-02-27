e32cf748fecfb4f0fa68018ce8997e606263f030
Rollie Ma <rollie.ma@gmail.com>
2015-02-26 22:56:46 -0800

    Fixed SEO meta tags and header title

--- routes/index.js
+++ routes/index.js
@@ -6,10 +6,10 @@ router.get('/', function(req, res) {
     res.render('index', {
         meta: {
             title: 'Rollie Ma - Polyglot Developer from Vancouver, BC',
-            description: 'Hi, I\'m Rollie Ma. A Linux lover and LEGO bricks enthusiast. \
-                A polyglot developer obsessed with PHP, Python and JavaScript. \
-                A receptive learner captivated by mobile development, NoSQL and data mining. \
-                An amateur explorer interested in information aggregation and artificial intelligence fields.',
+            description: 'Hi, I\'m Rollie Ma. A Linux lover and LEGO bricks enthusiast. ' +
+                'A polyglot developer obsessed with PHP, Python and JavaScript. ' +
+                'A receptive learner captivated by mobile development, NoSQL and data mining. ' +
+                'An amateur explorer interested in information aggregation and artificial intelligence fields.',
             url: 'http://rolli3.net',
             image: 'http://rolli3.net/images/logos/logo-120x120.png'
         }
