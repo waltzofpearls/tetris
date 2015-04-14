4b0c3df2eb46715f0c081dc12f948d5ed2b32444
Rollie Ma <rollie.ma@gmail.com>
2015-04-13 23:54:57 -0700

    Added less sourcemap

--- gulpfile.js
+++ gulpfile.js
@@ -6,9 +6,11 @@ var cleancss = new LessPluginCleanCSS({ advanced: true })
 
 gulp.task('less', function() {
     return gulp.src('public/stylesheets/style.less')
+        .pipe(plugins.sourcemaps.init())
         .pipe(plugins.less({
             plugins: [cleancss]
         }))
+        .pipe(plugins.sourcemaps.write())
         .pipe(gulp.dest('public/stylesheets'));
 });
 
