52596ae5e669040ea17e859ae275989c58d41320
Rollie Ma <rollie.ma@gmail.com>
2015-05-21 00:00:27 -0700

    Karma config WIP

--- tests/karma.conf.js
+++ tests/karma.conf.js
@@ -1,1 +1,33 @@
 'use strict';
+
+module.exports = function(config) {
+  config.set({
+    basePath: '../',
+
+    files: [
+      {pattern: 'public/javascripts/**/*.js', included: false},
+      {pattern: 'tests/unit-bjs/**/*_test.js', included: false},
+      'tests/test-main.js'
+    ],
+
+    exclude: [
+      'public/javascripts/main.js'
+      'public/javascripts/main.min.js'
+      'public/javascripts/libs/require-2.1.15/require.min.js'
+    ],
+
+    singleRun: false,
+    autoWatch: true,
+
+    frameworks: ['jasmine', 'requirejs'],
+
+    browsers: ['Chrome'],
+
+    plugins: [
+      'karma-chrome-launcher',
+      'karma-firefox-launcher',
+      'karma-jasmine',
+      'karma-requirejs'
+    ]
+  });
+};
