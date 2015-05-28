b7cbb09c91b047f6b41f1ae4d18970f361db1812
Rollie Ma <rollie.ma@gmail.com>
2015-05-27 23:56:07 -0700

    Added url tests to model spec files

--- tests/unit-bjs/models/ResumeModel.spec.js
+++ tests/unit-bjs/models/ResumeModel.spec.js
@@ -2,12 +2,27 @@
 
 define(['models/ResumeModel'], function(ResumeModel) {
   describe('Resume model', function() {
+    var model;
+
+    beforeEach(function() {
+      model = new ResumeModel({
+        title: 'Rollie Ma'
+      });
+    });
+
     describe('when instantiated', function() {
       it('should exhibit attributes', function() {
-        var resume = new ResumeModel({
-          title: 'Rollie Ma'
-        });
-        expect(resume.get('title')).toEqual('Rollie Ma');
+        expect(model.get('title')).toEqual('Rollie Ma');
+      });
+    });
+
+    describe('url', function() {
+      it("should set the URL to the collection URL", function() {
+        var collection = {
+          url: '/resume'
+        };
+        model.collection = collection;
+        expect(model.url()).toEqual(collection.url);
       });
     });
   });
--- tests/unit-bjs/models/projects/ProjectsListModel.spec.js
+++ tests/unit-bjs/models/projects/ProjectsListModel.spec.js
@@ -2,12 +2,27 @@
 
 define(['models/projects/ProjectsListModel'], function(ProjectsListModel) {
   describe('ProjectsList model', function() {
+    var model;
+
+    beforeEach(function() {
+      model = new ProjectsListModel({
+        title: 'Project Insight'
+      });
+    });
+
     describe('when instantiated', function() {
       it('should exhibit attributes', function() {
-        var projectsList = new ProjectsListModel({
-          title: 'Project Insight'
-        });
-        expect(projectsList.get('title')).toEqual('Project Insight');
+        expect(model.get('title')).toEqual('Project Insight');
+      });
+    });
+
+    describe('url', function() {
+      it("should set the URL to the collection URL", function() {
+        var collection = {
+          url: '/projects'
+        };
+        model.collection = collection;
+        expect(model.url()).toEqual(collection.url);
       });
     });
   });
