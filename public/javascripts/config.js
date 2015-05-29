297f6f39e24f325e0edac12d5d6c18417a334768
Rollie Ma <rollie.ma@gmail.com>
2015-05-28 23:49:14 -0700

    Took out require.config and put it into a separate file

---
+++ public/javascripts/config.js
@@ -0,0 +1,17 @@
+'use strict';
+
+require.config({
+    shim: {
+        'bootstrap': {deps: ['jquery']}
+    },
+    paths: {
+        'backbone': 'libs/backbone-1.1.2',
+        'bootstrap': 'libs/bootstrap-3.3.1',
+        'jquery': 'libs/jquery-1.11.1',
+        'marked': 'libs/marked-0.3.3',
+        'templates': '../templates',
+        'underscore': 'libs/underscore-1.7.0',
+        'domReady': 'libs/require-2.1.15/domReady',
+        'text': 'libs/require-2.1.15/text'
+    }
+});
