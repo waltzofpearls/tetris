9d6e3443495f11b054315d36a27e91a34d60ed9f
Rollie Ma <rollie.ma@gmail.com>
2015-05-29 00:56:54 -0700

    Consolidate config.js into test-main.js

--- tests/unit-bjs/test-main.js
+++ tests/unit-bjs/test-main.js
@@ -1,46 +1,39 @@
 'use strict';
 
-var tests = [];
-for (var file in window.__karma__.files) {
-  if (/spec\.js$/.test(file)) {
-    tests.push(file);
+require(['/base/public/javascripts/config.js'], function() {
+  var tests = [];
+  for (var file in window.__karma__.files) {
+    if (/spec\.js$/.test(file)) {
+      tests.push(file);
+    }
   }
-}
 
-requirejs.config({
-  // Karma serves files from '/base'
-  baseUrl: '/base/public/javascripts/',
+  requirejs.config({
+    // Karma serves files from '/base'
+    baseUrl: '/base/public/javascripts/',
 
-  shim: {
-    'bootstrap': {deps: ['jquery']},
-    'jasmine-sinon': {deps: ['sinon']},
-    'jasmine-jquery': {deps: ['jquery']},
-  },
+    shim: {
+      'jasmine-sinon': {deps: ['sinon']},
+      'jasmine-jquery': {deps: ['jquery']}
+    },
 
-  paths: {
-    'backbone': 'libs/backbone-1.1.2',
-    'bootstrap': 'libs/bootstrap-3.3.1',
-    'jquery': 'libs/jquery-1.11.1',
-    'sinon': 'libs/sinon-1.14.1',
-    'jasmine-sinon': 'libs/jasmine-sinon-0.4.0',
-    'jasmine-jquery': 'libs/jasmine-jquery-2.1.0',
-    'marked': 'libs/marked-0.3.3',
-    'templates': '../templates',
-    'underscore': 'libs/underscore-1.7.0',
-    'domReady': 'libs/require-2.1.15/domReady',
-    'text': 'libs/require-2.1.15/text'
-  },
+    paths: {
+      'sinon': 'libs/sinon-1.14.1',
+      'jasmine-sinon': 'libs/jasmine-sinon-0.4.0',
+      'jasmine-jquery': 'libs/jasmine-jquery-2.1.0'
+    },
 
-  // ask Require.js to load these files (all our tests)
-  deps: [
-    'jquery',
-    'underscore',
-    'backbone',
-    'sinon',
-    'jasmine-sinon',
-    'jasmine-jquery'
-  ].concat(tests),
+    // ask Require.js to load these files (all our tests)
+    deps: [
+      'jquery',
+      'underscore',
+      'backbone',
+      'sinon',
+      'jasmine-sinon',
+      'jasmine-jquery'
+    ].concat(tests),
 
-  // start test run, once Require.js is done
-  callback: window.__karma__.start
+    // start test run, once Require.js is done
+    callback: window.__karma__.start
+  });
 });
