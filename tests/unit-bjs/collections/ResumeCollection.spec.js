323ec809e80f518c9506b333551822be20129f2b
Rollie Ma <rollie.ma@gmail.com>
2015-05-30 01:02:38 -0700

    Collection tests

---
+++ tests/unit-bjs/collections/ResumeCollection.spec.js
@@ -0,0 +1,13 @@
+'use strict';
+
+define(['squire'], function(Squire) {
+  var injector = new Squire();
+  // var builder = injector
+  //   .mock('foo', {
+  //     message: "Hello, World!"
+  //   });
+
+  // injector
+  //   .require(['collections/ResumeCollection'], function(ResumeCollection) {
+  // });
+});
