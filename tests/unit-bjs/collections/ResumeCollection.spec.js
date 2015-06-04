e030110e65f0a8bcfb367376915859b0af45e489
Rollie Ma <rollie.ma@gmail.com>
2015-06-04 01:50:07 -0700

    ProjectsListCollection test

--- tests/unit-bjs/collections/ResumeCollection.spec.js
+++ tests/unit-bjs/collections/ResumeCollection.spec.js
@@ -3,19 +3,21 @@
 define(['squire'], function(Squire) {
   var injector = new Squire();
 
-  describe('Resume collection', function() {
+  describe('ResumeCollection', function() {
     var server, collection;
 
     beforeEach(function(done) {
-        injector
-          .mock('models/ResumeModel', {
-            message: 'Hello, World!'
-          })
-          .require(['collections/ResumeCollection'], function(ResumeCollection) {
-            server = sinon.fakeServer.create();
-            collection = new ResumeCollection();
-            done();
-          });
+      injector
+        .mock('models/ResumeModel', {
+          message: 'Hello, World!'
+        })
+        .require([
+          'collections/ResumeCollection'
+        ], function(ResumeCollection) {
+          server = sinon.fakeServer.create();
+          collection = new ResumeCollection();
+          done();
+        });
     });
 
     afterEach(function() {
@@ -24,12 +26,9 @@ define(['squire'], function(Squire) {
 
     it('should make the correct request', function() {
       collection.fetch();
-      expect(server.requests.length)
-        .toEqual(1);
-      expect(server.requests[0].method)
-        .toEqual('GET');
-      expect(server.requests[0].url)
-        .toEqual('/data/resume.json');
+      expect(server.requests.length).toEqual(1);
+      expect(server.requests[0].method).toEqual('GET');
+      expect(server.requests[0].url).toEqual('/data/resume.json');
     });
 
   });
--- tests/unit-bjs/collections/projects/ProjectsListCollection.spec.js
+++ tests/unit-bjs/collections/projects/ProjectsListCollection.spec.js
@@ -1,4 +1,36 @@
 'use strict';
 
-define(['collections/projects/ProjectsListCollection'], function(ProjectsListCollection) {
+define(['squire'], function(Squire) {
+  var injector = new Squire();
+
+  describe('ResumeCollection', function() {
+    var server, collection;
+
+    beforeEach(function(done) {
+      injector
+        .mock('models/projects/ProjectsListModel', {
+          message: 'Hello, World!'
+        })
+        .require([
+          'collections/projects/ProjectsListCollection'
+        ], function(ProjectsListCollection) {
+          server = sinon.fakeServer.create();
+          collection = new ProjectsListCollection();
+          done();
+        });
+    });
+
+    afterEach(function() {
+      server.restore();
+    });
+
+    it('should make the correct request', function() {
+      collection.fetch();
+      expect(server.requests.length).toEqual(1);
+      expect(server.requests[0].method).toEqual('GET');
+      expect(server.requests[0].url).toEqual('/api/projects');
+    });
+
+  });
+
 });
