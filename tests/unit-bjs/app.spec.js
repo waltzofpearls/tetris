cf8ee92f4608ae0855b58878d7474369e5e9cbeb
Rollie Ma <rollie.ma@gmail.com>
2015-06-05 00:35:01 -0700

    Fixed broken app test

--- tests/unit-bjs/app.spec.js
+++ tests/unit-bjs/app.spec.js
@@ -1,28 +1,34 @@
 'use strict';
 
 define(['squire'], function(Squire) {
-  var injector = new Squire();
+  var injector;
 
   describe('App entrypoint', function() {
-    var app;
 
-    beforeEach(function(done) {
+    beforeEach(function() {
+      injector = new Squire();
+    });
+
+    afterEach(function() {
+      injector.remove();
+    });
+
+    it('defined all the necessary properties', function(done) {
       injector.require(['app'], function(App) {
-        app = App;
+        expect(App.app).toBeDefined();
+        expect(App.tube).toBeDefined();
+        expect(App.initialize).toBeDefined();
         done();
       });
     });
 
-    it('defined all the necessary properties', function() {
-      expect(app.app).toBeDefined();
-      expect(app.tube).toBeDefined();
-      expect(app.initialize).toBeDefined();
+    it('can be properly initialized', function(done) {
+      injector.require(['app'], function(App) {
+        var isInstanceOf = (App.initialize() instanceof Backbone.Router);
+        expect(isInstanceOf).toBeTruthy();
+        done();
+      });
     });
-
-    // it('can be properly initialized', function() {
-    //   var isInstanceOf = (app.initialize() instanceof Backbone.Router);
-    //   expect(isInstanceOf).toBeTruthy();
-    // });
 
   });
 
