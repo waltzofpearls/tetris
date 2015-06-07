7b66b35d5c41c161a0642854d494c4a21bf78b25
Rollie Ma <rollie.ma@gmail.com>
2015-06-06 23:48:44 -0700

    Re-arranged collection test specs

--- tests/unit-bjs/collections/projects/ProjectsListCollection.spec.js
+++ tests/unit-bjs/collections/projects/ProjectsListCollection.spec.js
@@ -1,34 +1,46 @@
 'use strict';
 
 define(['squire'], function(Squire) {
-  var injector = new Squire();
 
   describe('ResumeCollection', function() {
-    var server, collection;
+    var injector;
 
-    beforeEach(function(done) {
-      injector
-        .mock('models/projects/ProjectsListModel', {
-          message: 'Hello, World!'
-        })
-        .require([
-          'collections/projects/ProjectsListCollection'
-        ], function(ProjectsListCollection) {
-          server = sinon.fakeServer.create();
-          collection = new ProjectsListCollection();
-          done();
-        });
+    beforeEach(function() {
+      injector = new Squire();
     });
 
     afterEach(function() {
-      server.restore();
+      injector.remove();
     });
 
-    it('should make the correct request', function() {
-      collection.fetch();
-      expect(server.requests.length).toEqual(1);
-      expect(server.requests[0].method).toEqual('GET');
-      expect(server.requests[0].url).toEqual('/api/projects');
+    describe('fetching models', function() {
+      var server, collection;
+
+      beforeEach(function(done) {
+        injector
+          .mock('models/projects/ProjectsListModel', {
+            message: 'Hello, World!'
+          })
+          .require([
+            'collections/projects/ProjectsListCollection'
+          ], function(ProjectsListCollection) {
+            server = sinon.fakeServer.create();
+            collection = new ProjectsListCollection();
+            done();
+          });
+      });
+
+      afterEach(function() {
+        server.restore();
+      });
+
+      it('should make the correct request', function() {
+        collection.fetch();
+        expect(server.requests.length).toEqual(1);
+        expect(server.requests[0].method).toEqual('GET');
+        expect(server.requests[0].url).toEqual('/api/projects');
+      });
+
     });
 
   });
