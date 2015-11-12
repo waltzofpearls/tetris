d8dc7c04b031e51fbe2d7bd8094f3562106e867b
Rollie Ma <rollie.ma@gmail.com>
2015-11-12 01:33:38 -0800

    Force requirejs to requirejs instead of require

--- public/javascripts/src/main.js
+++ public/javascripts/src/main.js
@@ -1,7 +1,7 @@
 'use strict';
 
-require(['config'], function() {
-    require(['app', 'ga'], function(App, Ga) {
+requirejs(['config'], function() {
+    requirejs(['app', 'ga'], function(App, Ga) {
         App.initialize();
         Ga('send', 'pageview');
     });
